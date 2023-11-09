9e78f76a098b550466189ac994788d851b79db56
msilis <mikssilis@gmail.com>
2023-11-09 09:49:14 +0000

    initial commit

---
+++ src/config/api.ts
@@ -0,0 +1,5 @@
+export const API_STATION_LIST =
+  "https://75750e17-4c6f-43f1-9a65-e4290c99700a.mock.pstmn.io/stations";
+
+export const API_STATION_DETAIL =
+  "https://75750e17-4c6f-43f1-9a65-e4290c99700a.mock.pstmn.io/station/";
