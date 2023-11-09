d4729aafb9d1d5af00f671004efdf5f846d47cba
msilis <mikssilis@gmail.com>
2023-11-09 12:03:34 +0000

    small logo styling

--- src/components/AudioContainer/AudioContainer.tsx
+++ src/components/AudioContainer/AudioContainer.tsx
@@ -1,12 +1,19 @@
 import style from "./AudioContainer.module.css";
 import smallLogo from "../../assets/global-logo-footer.svg";
 import { AudioPlayer } from "../AudioPlayer/AudioPlayer";
+import { StationContext } from "../../utils/stationContext";
+import { useContext } from "react";
 
 export const AudioContainer = () => {
+  const { stationDetail } = useContext(StationContext);
   return (
     <div className={style.audioContainer}>
       <div className={style.logoContainer}></div>
-      <img src={smallLogo} alt="logo" id={style.smallLogo} />
+      <img
+        src={stationDetail?.logo ? stationDetail.logo : smallLogo}
+        alt="logo"
+        id={style.smallLogo}
+      />
       <AudioPlayer />
     </div>
   );
